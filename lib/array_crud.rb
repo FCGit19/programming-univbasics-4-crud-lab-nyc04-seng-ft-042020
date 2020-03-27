c9437e9810f65e92d090dff383fecc302ce28ba0
Fred C <canofred@gmail.com>
2020-03-27 17:06:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   # array << 'arrays!' # or 
-  array.push('array!')
+  array.push('arrays!')
 end
 
 def add_element_to_start_of_array(array, element)
